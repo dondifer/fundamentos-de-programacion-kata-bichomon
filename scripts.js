f486c1f1cd88af88e17970b593bac7c59f75ff31
dondifer <dondifer@gmail.com>
2023-03-03 20:45:44 +0100

    feature: all pokimon painted

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 console.log(document.title);
+document.querySelector("#infocard-list").style.background =
+  "linear-gradient(90deg, rgba(0,4,255,1) 35%, rgba(255,0,0,1) 100%)";
+
+console.log(document.location.href);
+console.log(document.location.host);
+const img = document.getElementsByTagName("img");
+
+console.log(img);
+const arr = [...img];
+arr.forEach(function (item) {
+  item.src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+});
+
+const allFly = document.querySelectorAll(".itype.flying");
+const allFlyArr = [...allFly];
+allFlyArr.forEach((item) => {
+  item.parentNode.parentNode.style.background = "#9be0fe";
+});
